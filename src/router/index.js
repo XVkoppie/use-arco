5d4e5e40930478a4b571bb3ce6f2d59bc6de1280
xh卷心菜 <15144050+xh-cabbage@user.noreply.gitee.com>
2025-08-11 16:55:33 +0800

    完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   {path:'/',component:()=>import('@/views/home.vue')}
+   {path:'/',component:()=>import('@/views/Layout/index.vue')},
+   {path:'/login',component:()=>import('@/views/Login/index.vue')}
   ],
 })
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<a-layout class="layout">
+  <div>侧边栏</div>
+  <a-layout class="main">
+      <div>页头</div>
+    <a-layout-content class="content">
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+      <div>内容</div>
+    </a-layout-content>
+    <a-layout-footer>
+      <div>页脚</div>
+    </a-layout-footer>
+  </a-layout>
+</a-layout>
+</template>
+
+<style scoped lang="scss">
+.layout{
+  min-height: 100vh;
+  background-color: #ccc;
+}
+.main{
+  margin-left: 200px;
+  background-color: #ccc;
+}
+.content{
+  padding: 24px;
+  overflow-y: auto;
+  margin-top: 60px;
+  background-color: #fff;
+}
+</style>
