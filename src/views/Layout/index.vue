a4ae6b22e327f862c2ad5c7e214d6b477877ae09
xh卷心菜 <15144050+xh-cabbage@user.noreply.gitee.com>
2025-08-13 09:54:54 +0800

    基于路由动态生成菜单

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-
+import AppsideBar from './components/AppsideBar.vue'
 </script>
 
 <template>
 <a-layout class="layout">
-  <div>侧边栏</div>
+  <AppsideBar/>
   <a-layout class="main">
       <div>页头</div>
     <a-layout-content class="content">
-      <div>内容</div>
-      <div>内容</div>
-      <div>内容</div>
-      <div>内容</div>
-      <div>内容</div>
-      <div>内容</div>
-      <div>内容</div>
       <div>内容</div>
     </a-layout-content>
     <a-layout-footer>
@@ -27,12 +20,8 @@
 <style scoped lang="scss">
 .layout{
   min-height: 100vh;
-  background-color: #ccc;
 }
-.main{
-  margin-left: 200px;
-  background-color: #ccc;
-}
+
 .content{
   padding: 24px;
   overflow-y: auto;
